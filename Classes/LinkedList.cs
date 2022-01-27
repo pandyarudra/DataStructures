52cee6aba45e0d5d108dba55e91d238d5d76b525
Rudra Pandya <rudra.pandya@mcap.com>
2022-01-27 17:49:36 -0500

    Add project files.

---
+++ Classes/LinkedList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Classes
+{
+    public class LinkedList
+    {
+        public Node first; // head
+        public Node last; // tail
+
+        public LinkedList(Node first) 
+        {
+            this.first = first;
+            var current = first;
+            do
+            {
+                if (current.next == null)
+                {
+                    last = current;
+                }
+                current = current.next;
+            }
+            while (current != null);
+        }
+
+        public Node AddFirst(int val)
+        {
+            var newNode = new Node(val, first.next);
+            first = newNode;
+
+            return newNode;
+        }
+
+        public Node AddLast(int val)
+        {
+            var newNode = new Node(val, null);
+            last.next = newNode;       
+
+            return newNode;
+        }
+
+        public void DeleteFirst()
+        {
+            first = first.next;
+        }
+
+        public void DeleteLast()
+        {
+            var current = first;
+            do
+            {   // get last second node to assign next
+                if (current.next.next == null)
+                {
+                    current.next = null;
+                    last = current;
+                }
+            } while (current.next.next != null);
+        }
+
+        public int IndexOf(int val)
+        {
+            var index = 0;
+            var current = first;
+            while (current.val != val)
+            {
+                if (val == current.val)
+                {
+                    break;
+                } 
+                else
+                {
+                    current = current.next;
+                    index++;
+                }
+            }
+            return index;
+        }
+
+        public bool Contains(int val)
+        {
+            var current = first;
+            do
+            {
+                if (current.val == val)
+                {
+                    return true;
+                }
+                current = current.next;
+            } while (current.val != val);
+
+            return false;
+        }
+
+        public void Print()
+        {
+            var current = first;
+            do
+            {
+                Console.WriteLine(current.val + "\n");
+                current = current.next;
+            } while (current != null);
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node next;
+
+        public Node(int val, Node next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using DataStructures.Classes;
+using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNode = new Node(10);
+            firstNode.next = new Node(20);
+            firstNode.next.next = new Node(30);
+            firstNode.next.next.next = new Node(40);
+
+            var linkedList = new LinkedList(firstNode);
+            linkedList.DeleteFirst();
+            linkedList.Print();
+            Console.WriteLine(linkedList.Contains(40));
+            Console.ReadKey();
+        }
+    }
+}
